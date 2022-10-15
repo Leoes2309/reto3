34f4b0c43bb8c8040442595bd0133643c5b16f24
Leoes2309 <oscar.espinosa2.mt@usa.edu.co>
2022-10-15 17:04:30 -0500

    AdminService

--- src/main/java/com/ciclo3/reto3/service/AdminService.java
+++ src/main/java/com/ciclo3/reto3/service/AdminService.java
@@ -1,4 +1,62 @@
 package com.ciclo3.reto3.service;
 
+import com.ciclo3.reto3.entities.Admin;
+import com.ciclo3.reto3.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+    public List<Admin> getAll(){
+        return (List<Admin>) adminRepository.getAll();
+    }
+    public Optional<Admin> getAdmin(int id) {
+        return adminRepository.getAdmin(id);
+    }
+
+    public Admin save(Admin a){
+        if (a.getIdAdmin() == null){
+            return adminRepository.save(a);
+        } else {
+            Optional<Admin> adminEncontrado = adminRepository.getAdmin(a.getIdAdmin());
+            if (adminEncontrado.isEmpty()){
+                return adminRepository.save(a);
+            }else {
+                return a;
+            }
+        }
+    }
+    public Admin update(Admin a){
+        if (a.getIdAdmin() != null){
+            Optional<Admin> adminEncontrado = adminRepository.getAdmin(a.getIdAdmin());
+            if (!adminEncontrado.isEmpty()) {//aqui se verifica que no este vacio
+                if (a.getPassword() != null) {
+                    adminEncontrado.get().setPassword(a.getPassword());
+                }
+                /*
+                if (a.getEmail() != null){
+                    adminEncontrado.get().setEmail(a.getEmail());
+                }
+                 */
+                if (a.getName() != null){
+                    adminEncontrado.get().setName(a.getName());
+                }
+                return adminRepository.save(adminEncontrado.get());
+            }
+        }
+        return a;
+    }
+    public boolean deleteAdmin(int adminId){
+        Boolean resultado = getAdmin(adminId).map(adminPorEliminar ->{
+            adminRepository.delete(adminPorEliminar);
+            return true;
+        }).orElse(false);
+        return resultado;
+    }
+}
 }
